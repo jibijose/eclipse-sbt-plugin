6887b15295447638e18bd00089ad987379b1d855
Jibi Jose <jibijose@users.noreply.github.com>
2016-02-19 21:03:50 +0530

    cleanup1

--- eclipsesbtplugin.test/src/main/java/tycho/demo/itp/ApplicationTest.java
+++ eclipsesbtplugin.test/src/main/java/tycho/demo/itp/ApplicationTest.java
@@ -1,8 +1,11 @@
 package tycho.demo.itp;
 
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.junit.Test;
 
 public class ApplicationTest {
+	//static ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui.tests", "icons/sample.gif");
 
 	@Test
 	public void veryStupidTest() {
