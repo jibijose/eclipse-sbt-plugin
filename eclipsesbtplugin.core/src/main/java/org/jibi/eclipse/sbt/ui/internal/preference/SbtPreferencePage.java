a5e788795d9eb76153e58cb43ff0090e4190869b
Jibi Jose <jibijose@users.noreply.github.com>
2016-02-20 01:04:30 +0530

    sbt-preferences dummy code

---
+++ eclipsesbtplugin.core/src/main/java/org/jibi/eclipse/sbt/ui/internal/preference/SbtPreferencePage.java
@@ -0,0 +1,25 @@
+package org.jibi.eclipse.sbt.ui.internal.preference;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class SbtPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+	
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		
+		
+	}
+
+	
+	
+
+}
